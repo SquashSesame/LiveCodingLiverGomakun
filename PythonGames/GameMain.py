64899d5c0502e07335d4ec259432fdfc3bdaa457
Makoto Goto <m.goto@game4it.com>
2023-05-10 23:57:27 +0900

    - 2023/5/10 配信

--- PythonGames/GameMain.py
+++ PythonGames/GameMain.py
@@ -1,17 +1,22 @@
 import pygame
 import sys
 import time
+import random
 from pygame.locals import QUIT, KEYUP, KEYDOWN, K_SPACE, K_LEFT, K_RIGHT, K_UP, K_DOWN
 
 
 pygame.init()
 SURFACE = pygame.display.set_mode([800, 600])
+pygame.display.set_caption("Live Coding Liver")
 s_keymap = []
 s_objects = []
+s_imageList = {}
 
 
 class GObject:
-    def __init__(self, px, py):
+    def __init__(self, name, colsize, px, py):
+        self.name = name
+        self.col_size = colsize
         self.px = px
         self.py = py
         self.is_dead = False
@@ -25,6 +30,12 @@ class GObject:
         SURFACE.blit(self.image,
             (self.px - self.rect.centerx,
              self.py - self.rect.centery))
+
+    def onHit(self, attack):
+        pass
+    
+    def onDead(self):
+        pass
 
     def checkWithoutScreen(self):
         scrRect = SURFACE.get_rect()
@@ -47,10 +58,11 @@ class GObject:
 
 class Bullet(GObject):
     def __init__(self, px, py, spdx, spdy):
-        super().__init__(px, py)
+        super().__init__("bullet", 8, px, py)
         self.spdx = spdx
         self.spdy = spdy
-        self.image = pygame.image.load('img/bullet.png')
+        self.attack = 1
+        self.image = s_imageList['bullet'] # pygame.image.load('img/bullet.png')
         self.rect = self.image.get_rect()
     
     def update(self, deltaTime):
@@ -59,19 +71,45 @@ class Bullet(GObject):
         # 画面外へ行ったか？
         if self.checkWithoutScreen():
             self.is_dead = True
+        # 敵にあたったか？
+        hitEnemty = self.getHitEnemy()
+        if hitEnemty:
+            # 敵にあたった！！
+            self.is_dead = True
+            hitEnemty.onHit(self.attack)
+    
+    # あたった敵を取得
+    def getHitEnemy(self):
+        for obj in s_objects:
+            if obj == self:
+                continue
+
+            if obj.name == "enemy":
+                # 当たり判定をする
+                sx = obj.px - self.px
+                sy = obj.py - self.py
+                distance2 = sx * sx + sy * sy
+                checksize  = obj.col_size + self.col_size
+                checksize = checksize * checksize
+                if distance2 < checksize:
+                    return obj
+                
+        return None
+
+
 
 
 class Player(GObject):
     def __init__(self, px, py):
-        super().__init__(px, py)
-        self.image = pygame.image.load('img/player.png')
+        super().__init__("player", 16, px, py)
+        self.image = s_imageList['player'] #pygame.image.load('img/player.png')
         self.rect = self.image.get_rect()
         self.has_shoted = False
     
     def update(self, deltaTime):
         global s_keymap
         scrRect = SURFACE.get_rect()
-        self.speed = 100
+        self.speed = 200
         
         if K_LEFT in s_keymap:
             self.px -= self.speed * deltaTime
@@ -99,16 +137,84 @@ class Player(GObject):
                 # タマ出す
                 s_objects.append(
                     Bullet(self.px, self.py,
-                        0, -200))
+                        0, -400))
         else:
             self.has_shoted = False
 
 
+
+class CircleEffect(GObject):
+    def __init__(self, px, py):
+        super().__init__("effect", 0, px, py)
+        self.halfsz = 0
+        self.time = 3
+        self.end_radius = 100
+    
+    def update(self, deltaTime):
+        self.halfsz += self.end_radius * deltaTime * self.time
+        if self.halfsz > self.end_radius:
+            self.is_dead = True
+    
+    def draw(self):
+        pygame.draw.circle(
+            SURFACE,
+            (255,0,0),
+            (self.px, self.py),
+            self.halfsz,
+            1 )
+
+
+class EnemyUFO(GObject):
+    def __init__(self):
+        # 水平方向にランダムな位置に出現
+        super().__init__(
+            "enemy",
+            20,
+            random.random() * 600 + 100,
+            -32,
+        )
+        # グラフィックはUFO
+        self.image = s_imageList['enemyUFO'] #pygame.image.load('img/enemy_64x32_ufo.png')
+        self.rect = self.image.get_rect()
+        # 速度は下方向に等速移動
+        self.spdx = 0
+        self.spdy = 100 + random.random() * 100
+    
+    def update(self, deltaTime):
+        # スピード方向に移動
+        self.px += self.spdx * deltaTime
+        self.py += self.spdy * deltaTime
+
+        # 画面の下に消えたら自動削除        
+        scrRect = SURFACE.get_rect()
+        if self.py > (scrRect.bottom + self.rect.centery):
+            return True
+        
+    def onHit(self, attack):
+        #TODO: ダメージ計算を入れたい
+        self.onDead()
+    
+    def onDead(self):
+        #TODO： やられエフェクトを出して死亡
+        self.is_dead = True
+        s_objects.append(
+            CircleEffect(self.px, self.py)
+        )
+
+
 def main():
     # 初期化
+    s_imageList['player'] = pygame.image.load('img/player.png')
+    s_imageList['bullet'] = pygame.image.load('img/bullet.png')
+    s_imageList['enemyUFO'] = pygame.image.load('img/enemy_64x32_ufo.png')
+    
+    
     
     player = Player(400, 300)
     preTime = time.perf_counter()
+    
+    enemyTime = 0
+    
     
     ###################
     # メインループ
@@ -137,6 +243,14 @@ def main():
         deltaTime = curTime - preTime
         preTime = curTime
         # print("time ", curTime, " delta ", deltaTime)
+        
+        # 敵を出現させる
+        enemyTime += deltaTime
+        if enemyTime >= 1.0:
+            enemyTime = 0
+            s_objects.append(
+                EnemyUFO()
+            )
         
         
         # プレイヤーの移動
